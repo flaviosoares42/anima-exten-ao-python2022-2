84bd244cad5e7a7c5d3bee21fc5e03ed0f7e732b
Soares Meireles <103222820+flaviosoares42@users.noreply.github.com>
2022-10-20 00:53:18 +0000

    aprendi a criar variáveis e exibi-las na tela

---
+++ main.py
@@ -0,0 +1,11 @@
+# meu primeiro projeto python!!!
+#
+#print()=comando de saida
+print("Alo mundo")
+
+#quando quiser guardar uma string!(frase)
+nome="Flavio Soares"
+#quando quiser gravar um numero inteiro
+idade=43
+print(nome)
+print(idade)
